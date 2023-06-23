47c18faf022fcd259e72c1539a1dd24f0b867f14
ultraclicker <97359361+superclicker10@users.noreply.github.com>
2023-06-23 23:07:38 +0100

    Started interact and added ending 1
    Interact stuff is commented out because it's buggy

--- Python Computer MK.25 - Testing.py
+++ Python Computer MK.25 - Testing.py
@@ -9,6 +9,10 @@ game_on = False
 x, newx = 0, 0
 y, newy = 0, 0
 room = "lobby"
+inventory = []
+ending_condition = False
+door_interact_state = False
+bookshelf_interact_state = False
 
 def lobby_sequence():
     global room
@@ -62,6 +66,40 @@ def book_room_examine(obj_examine):
     else:
         print("You can't examine that.")
         time.sleep(1)
+"""
+def book_room_door_interact(door_interact):
+    if door_interact == False:
+        print("You open the door wider, giving you a clear view of the corridor ahead.")
+        time.sleep(1)
+        print("Even though you already could've gone through it.")
+        time.sleep(1)
+        door_interact = True
+        return door_interact
+    else:
+        print("You close the door back to where it was originally.")
+        time.sleep(1)
+        door_interact = False
+        return door_interact
+
+def book_room_bookshelf(bookshelf_interact):
+    if bookshelf_interact == False:
+        print("You take a book from one of the shelves, hoping it might be useful.")
+        time.sleep(1)
+        global bookshelf_interact_state
+        bookshelf_interact_state = True
+        add_inventory("book")
+        return bookshelf_interact
+    else:
+        print("You have no need for another book.")
+        time.sleep(1)
+"""
+def hole_ending_sequence():
+    global ending_condition
+    print("You jump in the hole, falling forever and ever.")
+    time.sleep(3)
+    print("ENDING 1 - FALLING INTO THE DEPTHS")
+    time.sleep(2)
+    ending_condition = True
 
 def one_one_corridor():
     global room
@@ -82,7 +120,7 @@ def one_one_corridor_examine(obj_examine):
     elif obj_examine == "wall":
         print("These are unusually thick stone walls.")
         time.sleep(1)
-        print("You see no way to exit at it is.")
+        print("You see no way to exit as it is.")
         time.sleep(1)
     else:
         print("You can't examine that.")
@@ -164,7 +202,52 @@ def examine(room):
     elif room == "one_one_corridor":
         obj_examine = input("What object would you like to examine?: ")
         one_one_corridor_examine(obj_examine)
-    
+"""
+def interact(room):
+    obj_interact = input("What would you like to interact with?: ")
+    if room == "lobby":
+        if obj_interact == "wall":
+            print("You touch the wall. It's cold.")
+            time.sleep(1)
+        else:
+            print("You can't interact with that.")
+    elif room == "book_room":
+        if obj_interact == "door":
+            book_room_door_interact(door_interact_state)
+        elif obj_interact == "bookshelf":
+            book_room_bookshelf(bookshelf_interact_state)
+        elif obj_interact == "hole":
+            hole_ending_sequence()
+        else:
+            print("You can't interact with that. ")
+"""
+def inventory_general():
+    choice = input("View or drop item from inventory?: ")
+    if choice == "view":
+        print(inventory)
+    elif choice == "drop":
+        item = input("What item would you like to drop?: ")
+        drop_item(item)
+
+def add_inventory(item):
+    if len(inventory) == 3:
+        print("You cannot add this item, as you haven't enough inventory space.")
+        time.sleep(1)
+        print("Drop an item, or use one first.")
+        time.sleep(1)
+    else:
+        print("Added", item, "to inventory!")
+        inventory.append(item)
+
+def drop_item(item):
+    for x in inventory:
+        if item == x:
+            inventory.pop(item)
+            print("Dropped", item, "from inventory!")
+            time.sleep(1)
+        else:
+            pass
+    print("You do not have this item.")
 
 def room_check(x, y, newx, newy):
     for x in locations:
@@ -192,11 +275,16 @@ def repeated_action(x, y, newx, newy):
     x = newx
     y = newy
     print("Your co-ordinates are", str(x)+", "+str(y))
-    action = input("What do? (interact, move, use, examine): ")
+    action = input("What do? (interact, move, use, examine, inventory): ")
     if action == "move":
         move(x, y, room)
     elif action == "examine":
         examine(room)
+"""    elif action == "interact":
+        interact(room)
+"""
+    elif action == "inventory":
+        inventory_general()
     
 
 while game_on == False:
@@ -206,8 +294,13 @@ while game_on == False:
         game_on = True
     else:
         print("Next time, play gaming.")
+        game_on = "ended"
+        break
 
 while game_on == True:
+    if ending_condition == True:
+        game_on = "ended"
+        break
     repeated_action(x, y, newx, newy)
     """
     try:
--- locations.py
+++ locations.py
@@ -6,3 +6,7 @@ corridor = location(1, 1, False, False, False, True)
 #book room has edges north, east, and south-east leading to door, bookshelf and hole respectively
 
 locations = [lobby, book_room, corridor]
+
+door_interact = False
+bookshelf_interact = False
+
