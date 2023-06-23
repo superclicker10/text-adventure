551c09d52c62e8c162cef8a7efc95bb8e4210eb6
ultraclicker <97359361+superclicker10@users.noreply.github.com>
2023-06-23 22:20:46 +0100

    Fixed movement completely and added examine
    Added examine for the amount of content there is so far.
    Also fixed a movement bug which was caused by me not finishing a piece of code in the last section (doofus me)

--- Python Computer MK.25 - Testing.py
+++ Python Computer MK.25 - Testing.py
@@ -29,6 +29,65 @@ def book_room():
     time.sleep(1)
     print("There is a large hole at your south-east.")
 
+def bookshelf_sequence():
+    print("Test bookshelf")
+    time.sleep(1)
+
+def corner_hole():
+    print("The infinite hole.")
+    time.sleep(1)
+
+def book_room_door():
+    print("There is a door leading to your north (+y) direction leading to a long corridor.")
+    time.sleep(1)
+
+def book_room_examine(obj_examine):
+    if obj_examine == "bookshelf":
+        print("There are only a few books on this moldy-looking bookshelf.")
+        time.sleep(1)
+        print("You turn a few pages inside some, but there appears to be random words scattered across the pages.")
+        time.sleep(1)
+        print("You could take a book, but you aren't sure what use it would serve.")
+        time.sleep(1)
+    elif obj_examine == "hole":
+        print("You look down the hole.")
+        time.sleep(1)
+        print("It goes on for miles and miles, with a calling inside your head telling you something will be at the bottom.")
+        time.sleep(1)
+    elif obj_examine == "door":
+        print("The door is slightly ajar.")
+        time.sleep(1)
+        print("You see a corridor ahead of you that is longer than the confined spaces you are in.")
+        time.sleep(1)
+    else:
+        print("You can't examine that.")
+        time.sleep(1)
+
+def one_one_corridor():
+    global room
+    room = "one_one_corridor"
+    print("You are in a corridor.")
+    time.sleep(1)
+    print("There is a door ahead of you by 2 spaces.")
+    time.sleep(1)
+    print("The door you came through is behind you.")
+    time.sleep(1)
+    print("There is no sign of anything useful in this space.")
+    time.sleep(1)
+
+def one_one_corridor_examine(obj_examine):
+    if obj_examine == "door":
+        print("Going back to the last room would allow you to get a better look at the door.")
+        time.sleep(1)
+    elif obj_examine == "wall":
+        print("These are unusually thick stone walls.")
+        time.sleep(1)
+        print("You see no way to exit at it is.")
+        time.sleep(1)
+    else:
+        print("You can't examine that.")
+        time.sleep(1)
+
 def oob(x, y, newx, newy):
     if newx <= -1 or newy <= -1:
         print("All negative co-ordinates are out of bounds.")
@@ -95,18 +154,17 @@ def move(x, y, room):
                 corner_hole()
         else:
             print("There are no edges to move to in this room.")
-            
-def bookshelf_sequence():
-    print("Test bookshelf")
-    time.sleep(1)
 
-def corner_hole():
-    print("The infinite hole.")
-    time.sleep(1)
-
-def book_room_door():
-    print("There is a door leading to your north (+y) direction leading to a long corridor.")
-    time.sleep(1)
+def examine(room):
+    if room == "lobby":
+        print("There is nothing to examine in this room.")
+    elif room == "book_room":
+        obj_examine = input("What object would you like to examine?: ")
+        book_room_examine(obj_examine)
+    elif room == "one_one_corridor":
+        obj_examine = input("What object would you like to examine?: ")
+        one_one_corridor_examine(obj_examine)
+    
 
 def room_check(x, y, newx, newy):
     for x in locations:
@@ -119,7 +177,9 @@ def room_check(x, y, newx, newy):
             book_room()
             break
         elif newx == 1 and newy == 1:
-            co
+            one_one_corridor()
+            break
+
 """
 def first_action(x, y, counter):
     room_check(x, y, 0, 0)
@@ -134,7 +194,9 @@ def repeated_action(x, y, newx, newy):
     print("Your co-ordinates are", str(x)+", "+str(y))
     action = input("What do? (interact, move, use, examine): ")
     if action == "move":
-        move(x, y, room)      
+        move(x, y, room)
+    elif action == "examine":
+        examine(room)
     
 
 while game_on == False:
@@ -146,8 +208,13 @@ while game_on == False:
         print("Next time, play gaming.")
 
 while game_on == True:
+    repeated_action(x, y, newx, newy)
+    """
     try:
         repeated_action(x, y, newx, newy)
     except NameError:
-        newx = 0
-        newy = 0
+        print("The game works, don't worry bout it.")
+        x = newx
+        y = newy
+    """
+    
